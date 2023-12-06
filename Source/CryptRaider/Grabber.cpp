569d07df64d224c094f41d03bcfc230c72eb6353
youngil <youngil@placeb.com>
2023-12-06 09:41:38 +0900

    DrawDebugLine

--- Source/CryptRaider/Grabber.cpp
+++ Source/CryptRaider/Grabber.cpp
@@ -3,6 +3,7 @@
 
 #include "Grabber.h"
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,16 +31,10 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FRotator MyRotation = GetComponentRotation();
-	FString RotationString = MyRotation.ToString();
-	UE_LOG(LogTemp, Display, TEXT("Grabber Rotation : %s"), *RotationString);
-
-	// ...
-
-	//게임시간..
-	float TimeSeconds = GetWorld()->TimeSeconds;
-
-	UE_LOG(LogTemp, Display, TEXT("TimeSeconds : %f"), TimeSeconds);
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
 	
 }
 
